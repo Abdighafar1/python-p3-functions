96a7c67a2e9bf062716b772bb3e3fe1cf54209e3
Abdighafar1 <abdighafar.ali@student.moringaschool.com>
2024-03-01 19:01:22 +0300

    Done with assignments

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,49 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f'Hello, {name}!')
+greet('laith')
 
 def greet_with_default(name="programmer"):
-    pass
+    print(F'Hello, {name}!')
+greet_with_default()
+greet_with_default('Laith')
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+sum = add(1,2)
+print(sum)
 
 def halve(number):
-    pass
+    return number / 2
+
+
+def my_function(param):
+    print('running my function')
+    return param + 1
+
+my_function_return_value = my_function(1)
+print(my_function_return_value)
+
+def say_hi(name):
+    print(f"Hi there, {name}!")
+
+say_hi('laith')
+
+def say_hi(name="Engineer"):
+    print(f"Hi there, {name}!")
+
+say_hi()
+say_hi('laith')
+
+def stylish_painter():
+    best_hairstyle = "Bob Ross"
+    return "Jean-Michel Basquiat"
+    return best_hairstyle
+    print(best_hairstyle)
+
+stylish_painter()
